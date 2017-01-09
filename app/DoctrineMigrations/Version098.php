803a3562424fa242db2815ed020ee4e1c576187c
Phil Shean <sheanieuk@gmail.com>
2017-01-09 12:11:03 +0000

    Migration Fix
    Fixed class name error.

--- app/DoctrineMigrations/Version098.php
+++ app/DoctrineMigrations/Version098.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version097 extends AbstractMigration
+class Version098 extends AbstractMigration
 {
     /**
      * @param Schema $schema
