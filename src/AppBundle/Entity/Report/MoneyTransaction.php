ff98080d0fa1099bb50f97c0560bcb5a7cc8a038
Shaun Lizzio <shaun@lizzio.co.uk>
2018-03-26 15:57:02 +0100

    DDPB-2032 Move Compensation/Damages to group

--- src/AppBundle/Entity/Report/MoneyTransaction.php
+++ src/AppBundle/Entity/Report/MoneyTransaction.php
@@ -61,11 +61,9 @@ class MoneyTransaction implements MoneyTransactionInterface
             ],
             'config' => ['hasDetails' => false, 'type' => 'in']
         ],
-        'damages' => [
-            'categories' => [
-                'compensation-or-damages-award' => ['config' => ['hasDetails' => true, 'type' => 'in']]
-            ],
-            'config' => ['hasDetails' => false, 'type' => 'in']
+        'compensation-or-damages-award' => [
+            'categories' => [],
+            'config' => ['hasDetails' => true, 'type' => 'in']
         ],
         'one-off' => [
             'categories' => [
