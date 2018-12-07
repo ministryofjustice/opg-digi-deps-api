9fd58260b8cdd13eaa2a5712ade81e6d017eeb59
Alex Eves <alex.eves@aiesoftware.com>
2018-12-07 11:43:05 +0000

    DDPB-2359: Updates ReportSubmissionRepository->findAllReportSubmissions signature to accept DateTime objects

--- src/AppBundle/Controller/Report/ReportSubmissionController.php
+++ src/AppBundle/Controller/Report/ReportSubmissionController.php
@@ -146,18 +146,28 @@ class ReportSubmissionController extends RestController
      */
     public function getCasrecData(Request $request)
     {
-        $repo = $this->getRepository(EntityDir\Report\ReportSubmission::class); /* @var $repo EntityDir\Repository\ReportSubmissionRepository */
+        /* @var $repo EntityDir\Repository\ReportSubmissionRepository */
+        $repo = $this->getRepository(EntityDir\Report\ReportSubmission::class);
 
         $ret = $repo->findAllReportSubmissions(
             $request->get('offset', 0),
             $request->get('limit', 100),
-            $request->get('fromDate', []),
-            $request->get('toDate', []),
+            $this->convertDateArrayToDateTime($request->get('fromDate', [])),
+            $this->convertDateArrayToDateTime($request->get('toDate', [])),
             $request->get('orderBy', 'createdOn'),
             $request->get('order', 'ASC')
 
         );
 
         return $this->get('report_submission_stat_service')->generateReportSubmissionsCsvLines($ret);
+    }
+
+    /**
+     * @param array $date
+     * @return \DateTime|null
+     */
+    private function convertDateArrayToDateTime(array $date)
+    {
+        return (isset($date['date'])) ? new \DateTime($date['date']) : null;
     }
 }
