36ff86fe5725bb9610e428cf8ab7abe60a226cc0
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2019-05-17 11:24:28 +0100

    Update script directories and users

--- scripts/resetdb.sh
+++ scripts/resetdb.sh
@@ -3,8 +3,8 @@ set -e
 # We need below to create the params file on container start
 run-parts /etc/my_init.d
 
-. /app/scripts/initialize_schema.sh
+. /var/www/scripts/initialize_schema.sh
 
-/sbin/setuser app php app/console doctrine:migrations:status-check
-/sbin/setuser app php app/console doctrine:migrations:migrate --no-interaction -vvv
-/sbin/setuser app php app/console doctrine:fixtures:load --no-interaction
+php app/console doctrine:migrations:status-check
+php app/console doctrine:migrations:migrate --no-interaction -vvv
+php app/console doctrine:fixtures:load --no-interaction
