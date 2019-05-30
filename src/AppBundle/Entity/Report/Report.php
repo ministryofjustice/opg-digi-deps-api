8a944a026d973f3a5358f8a02a4547d944247d19
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2019-05-30 11:06:27 +0100

    Add `not_deputy` option to declaration question

--- src/AppBundle/Entity/Report/Report.php
+++ src/AppBundle/Entity/Report/Report.php
@@ -336,7 +336,7 @@ class Report implements ReportInterface
     private $reportSeen;
 
     /**
-     * @var string only_deputy|more_deputies_behalf|more_deputies_not_behalf
+     * @var string not_deputy|only_deputy|more_deputies_behalf|more_deputies_not_behalf
      *
      * @JMS\Type("string")
      * @JMS\Groups({"report"})
@@ -890,7 +890,7 @@ class Report implements ReportInterface
 
     public function setAgreedBehalfDeputy($agreeBehalfDeputy)
     {
-        $acceptedValues = ['only_deputy', 'more_deputies_behalf', 'more_deputies_not_behalf'];
+        $acceptedValues = ['not_deputy', 'only_deputy', 'more_deputies_behalf', 'more_deputies_not_behalf'];
         if ($agreeBehalfDeputy && !in_array($agreeBehalfDeputy, $acceptedValues)) {
             throw new \InvalidArgumentException(__METHOD__ . " {$agreeBehalfDeputy} given. Expected value: " . implode(' or ', $acceptedValues));
         }
